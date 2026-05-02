268a94cd80fef967de63ccb1281fcd526699a628
Amir Mobasheraghdam <142031484+Amirmobash@users.noreply.github.com>
2026-05-02 03:57:10 -1100

    Update RGB-Code.ino

--- RGB-Code.ino
+++ RGB-Code.ino
@@ -1,107 +1,107 @@
-const byte redPin = 11;
-const byte greenPin = 9;
-const byte bluePin = 5;
+const byte RED_PIN = 11;
+const byte GREEN_PIN = 9;
+const byte BLUE_PIN = 5;
 
 void setup() {
-  pinMode(redPin, OUTPUT);
-  pinMode(greenPin, OUTPUT);
-  pinMode(bluePin, OUTPUT);
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(BLUE_PIN, OUTPUT);
 }
 
 void loop() {
-  transition(2);
-  transition(2);
-  transition(2);
+  fadeColors(2);
+  fadeColors(2);
+  fadeColors(2);
 
-  quickChange(300);
-  quickChange(300);
-  quickChange(300);
+  flashColors(300);
+  flashColors(300);
+  flashColors(300);
 
-  transition(10);
+  fadeColors(10);
 
-  quickChange(100);
-  quickChange(100);
-  quickChange(100);
-  quickChange(100);
-  quickChange(100);
+  flashColors(100);
+  flashColors(100);
+  flashColors(100);
+  flashColors(100);
+  flashColors(100);
 }
 
-void allOff() {
-  setColor(0, 0, 0);
+void flashColors(int waitTime) {
+  setRgbColor(255, 255, 255);
+  delay(waitTime);
+
+  setRgbColor(0, 255, 255);
+  delay(waitTime);
+
+  setRgbColor(255, 0, 255);
+  delay(waitTime);
+
+  setRgbColor(255, 255, 0);
+  delay(waitTime);
+
+  setRgbColor(0, 0, 255);
+  delay(waitTime);
+
+  setRgbColor(0, 255, 0);
+  delay(waitTime);
+
+  setRgbColor(255, 0, 0);
+  delay(waitTime);
 }
 
-void quickChange(int pause) {
-  setColor(255, 255, 255);
-  delay(pause);
+void fadeColors(int waitTime) {
+  int redValue = 255;
+  int greenValue = 0;
+  int blueValue = 0;
 
-  setColor(0, 255, 255);
-  delay(pause);
-
-  setColor(255, 0, 255);
-  delay(pause);
-
-  setColor(255, 255, 0);
-  delay(pause);
-
-  setColor(0, 0, 255);
-  delay(pause);
-
-  setColor(0, 255, 0);
-  delay(pause);
-
-  setColor(255, 0, 0);
-  delay(pause);
-}
-
-void transition(int pause) {
-  int red = 255;
-  int green = 0;
-  int blue = 0;
-
-  while (blue < 255) {
-    blue++;
-    setColor(red, green, blue);
-    delay(pause * 3);
+  while (blueValue < 255) {
+    blueValue++;
+    setRgbColor(redValue, greenValue, blueValue);
+    delay(waitTime * 3);
   }
 
-  while (red > 0) {
-    red--;
-    setColor(red, green, blue);
-    delay(pause * 2);
+  while (redValue > 0) {
+    redValue--;
+    setRgbColor(redValue, greenValue, blueValue);
+    delay(waitTime * 2);
   }
 
-  while (green < 255) {
-    green++;
-    setColor(red, green, blue);
-    delay(pause);
+  while (greenValue < 255) {
+    greenValue++;
+    setRgbColor(redValue, greenValue, blueValue);
+    delay(waitTime);
   }
 
-  while (blue > 0) {
-    blue--;
-    setColor(red, green, blue);
-    delay(pause);
+  while (blueValue > 0) {
+    blueValue--;
+    setRgbColor(redValue, greenValue, blueValue);
+    delay(waitTime);
   }
 
-  while (red < 255) {
-    red++;
-    setColor(red, green, blue);
-    delay(pause * 2);
+  while (redValue < 255) {
+    redValue++;
+    setRgbColor(redValue, greenValue, blueValue);
+    delay(waitTime * 2);
   }
 
-  while (green > 0) {
-    green--;
-    setColor(red, green, blue);
-    delay(pause * 3);
+  while (greenValue > 0) {
+    greenValue--;
+    setRgbColor(redValue, greenValue, blueValue);
+    delay(waitTime * 3);
   }
 }
 
-void setColor(int red, int green, int blue) {
-  analogWrite(redPin, limit(red));
-  analogWrite(greenPin, limit(green));
-  analogWrite(bluePin, limit(blue));
+void turnOffLed() {
+  setRgbColor(0, 0, 0);
 }
 
-int limit(int value) {
+void setRgbColor(int redValue, int greenValue, int blueValue) {
+  analogWrite(RED_PIN, keepInRange(redValue));
+  analogWrite(GREEN_PIN, keepInRange(greenValue));
+  analogWrite(BLUE_PIN, keepInRange(blueValue));
+}
+
+int keepInRange(int value) {
   if (value < 0) return 0;
   if (value > 255) return 255;
   return value;
